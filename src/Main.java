e34fb26e6b54fb9554fec5715f60bca94d4a0bdf
Пользователь <woodoo08@mail.ru>
2022-10-20 21:13:48 +0300

    Проверьте пожалуйста ДЗ.

--- src/Main.java
+++ src/Main.java
@@ -30,6 +30,10 @@ public class Main {
         System.out.println("Средняя сумма трат за месяц составила " + averageSpend + " рублей!");
 
         System.out.println("ЗАДАНИЕ 4");
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
 
     }
 
